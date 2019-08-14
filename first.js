c4adda79d3a267b9b666376f938abf08ba2d9596
yaleksandrova21 <54073652+yaleksandrova21@users.noreply.github.com>
2019-08-14 17:03:58 +0300

    Переписать if'ы в switch

--- first.js
+++ first.js
@@ -194,3 +194,28 @@ switch (browser) {
       alert('Мы надеемся, что и в вашем браузере все ок!');
   }
 
+//Переписать if'ы в switch
+var a = +prompt('a?', '');
+if (a == 0) {
+  alert( 0 );
+}
+if (a == 1) {
+  alert( 1 );
+}
+if (a == 2 || a == 3) {
+  alert( '2,3' );
+}
+
+//Ответ:
+switch (a = +prompt('a?', '')) {
+    case 0:
+        alert (0);
+        break;
+    case 1:
+        alert(1);
+        break;
+    case 2:
+    case 3:
+        alert ('2,3');
+        break;
+}
