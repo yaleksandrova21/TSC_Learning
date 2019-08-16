3b9e40ec42de60bbb4fc46845c6c57f0f5324b08
yaleksandrova21 <54073652+yaleksandrova21@users.noreply.github.com>
2019-08-16 11:09:40 +0300

    Update three.js

--- three.js
+++ three.js
@@ -8,3 +8,10 @@ function truncate(str, maxlength) {
 truncate('Добрый день турутутуту', 5);
 truncate('Добрый день турутутуту', 50);
 
+//Есть стоимость в виде строки: "$120". То есть, первым идёт знак валюты, а затем – число.
+//Создайте функцию extractCurrencyValue(str), которая будет из такой строки выделять число-значение, в данном случае 120.
+function extractCurrencyValue(str) {
+    var a = str.slice(1,-1) + str.slice(-1);
+    return +a;
+}
+extractCurrencyValue('$120')
