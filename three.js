f153a946b9db3924bfcd333f596a9e09ddda701f
yaleksandrova21 <54073652+yaleksandrova21@users.noreply.github.com>
2019-08-16 13:17:58 +0300

    Update three.js

--- three.js
+++ three.js
@@ -15,3 +15,93 @@ function extractCurrencyValue(str) {
     return +a;
 }
 extractCurrencyValue('$120')
+
+/* Мини-задача на синтаксис объектов. Напишите код, по строке на каждое действие.
+Создайте пустой объект user.
+Добавьте свойство name со значением Вася.
+Добавьте свойство surname со значением Петров.
+Поменяйте значение name на Сергей.
+Удалите свойство name из объекта. */
+var user = {};
+user.name = 'Вася';
+user.surname = 'Петров'
+user.name = 'Сергей';
+delete user.name;
+
+//Создайте функцию isEmpty(obj), которая возвращает true, если в объекте нет свойств и false – если хоть одно свойство есть.
+function isEmpty(obj) {
+    for (var key in obj) {
+        return false;
+    } 
+    return true;
+  }
+  
+  var schedule = new Object();
+  alert( isEmpty(schedule) ); // true
+  schedule["8:30"] = "подъём";
+  alert( isEmpty(schedule) ); // false
+
+/* Есть объект salaries с зарплатами. Напишите код, который выведет сумму всех зарплат.
+Если объект пустой, то результат должен быть 0 */
+var salaries = {
+    "Вася": 100,
+    "Петя": 300,
+    "Даша": 250
+  };
+  var sum = salaries["Вася"] + salaries["Петя"] + salaries["Даша"]; 
+  if (sum > 0) {
+    alert(sum);
+  } else {
+    alert(0);
+  }
+
+//Есть объект salaries с зарплатами. Напишите код, который выведет имя сотрудника, у которого самая большая зарплата.
+//Еcли объект пустой, то пусть он выводит «нет сотрудников».
+var salaries = {
+    "Вася": 100,
+    "Петя": 300,
+    "Даша": 250
+  };
+  var max = 0;
+  var name = '';
+for (var key in salaries) {
+      if (max < salaries[key]) {
+          max = salaries[key];
+          name = key;
+      } 
+    } 
+alert(name);
+if (max == 0) {
+    alert('Нет сотрудников');
+}
+
+//Создайте функцию multiplyNumeric, которая получает объект и умножает все численные свойства на 2. Например:
+/* // до вызова
+var menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+multiplyNumeric(menu);
+
+// после вызова
+menu = {
+  width: 400,
+  height: 600,
+  title: "My menu"
+}; */
+var menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+  function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n)
+  }
+function multiplyNumeric(obj) {
+      for (var key in obj) {
+        if (isNumeric(obj[key])) {
+            obj[key] = obj[key] * 2;
+        }
+      }
+}
