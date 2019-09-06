b3baf9752b315c1afdb71f1761a818a3fa914d66
yaleksandrova21 <54073652+yaleksandrova21@users.noreply.github.com>
2019-09-06 16:51:40 +0300

    Update six.js

--- six.js
+++ six.js
@@ -39,3 +39,28 @@ function printNumbers(a, b) {
         a++;
     }, 1000);
 }
+
+//Вызов askPassword() в приведённом ниже коде должен проверить пароль и затем вызвать user.loginOk/loginFail в зависимости от ответа.
+//Исправьте выделенную строку, чтобы всё работало (других строк изменять не надо).
+
+function askPassword(ok, fail) {
+    let password = prompt("Password?", '');
+    if (password == "rockstar") ok();
+    else fail();
+  }
+  
+  let user = {
+    name: 'Вася',
+  
+    loginOk() {
+      alert(`${this.name} logged in`);
+    },
+  
+    loginFail() {
+      alert(`${this.name} failed to log in`);
+    },
+  
+  };
+  
+// askPassword(user.loginOk, user.loginFail);
+askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
