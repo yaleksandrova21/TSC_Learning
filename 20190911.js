ed8be5d0009d7cccc8c63748c71a6ea898033a42
yaleksandrova21 <54073652+yaleksandrova21@users.noreply.github.com>
2019-09-11 15:54:33 +0300

    Update 20190911.js

--- 20190911.js
+++ 20190911.js
@@ -9,3 +9,20 @@ var userJSON = JSON.stringify(user);
 alert (userJSON);
 var userObj = JSON.parse(userJSON);
 alert(userObj);
+
+//Напишите функцию replacer для JSON-преобразования, которая удалит свойства, ссылающиеся на meetup:
+let room = {
+    number: 23
+};
+let meetup = {
+    title: "Совещание",
+    occupiedBy: [{name: "Иванов"}, {name: "Петров"}],
+    place: room
+};
+  
+room.occupiedBy = meetup;
+meetup.self = meetup;
+  
+alert( JSON.stringify(meetup, function replacer(key, value) {
+    return (key != "" && value == meetup) ? undefined : value;
+}));
