d41c544a4e36b1663ad8f51350fefbd88a5686f2
yaleksandrova21 <54073652+yaleksandrova21@users.noreply.github.com>
2019-09-11 17:10:39 +0300

    Update 20190911.js

--- 20190911.js
+++ 20190911.js
@@ -46,3 +46,30 @@ let animal = {
   delete animal.jumps;
   
   alert( rabbit.jumps ); // ? (3) undefined
+
+/* Алгоритм поиска
+С помощью свойства __proto__ задайте прототипы так, чтобы поиск любого свойства выполнялся по следующему пути: 
+pockets → bed → table → head. 
+Например, pockets.pen должно возвращать значение 3 (найденное в table), а bed.glasses – значение 1 (найденное в head).
+Ответьте на вопрос: как быстрее получить значение glasses – через pockets.glasses или через head.glasses? 
+При необходимости составьте цепочки поиска и сравните их.*/
+let head = {
+    glasses: 1
+};
+  
+let table = {
+    pen: 3
+};
+  
+let bed = {
+    sheet: 1,
+    pillow: 2
+};
+
+let pockets = {
+    money: 2000
+};
+
+pockets.__proto__ = bed;
+bed.__proto__ = table;
+table.__proto__ = head;
